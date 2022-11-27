ca5c935cc02767c4f0989762e968af6e741b8868
tranthien727 <tran.thien727@gmail.com>
2022-11-27 20:18:38 +0700

    addtimkiem

--- home.php
+++ home.php
@@ -31,7 +31,64 @@
         </script>
         <table align="center" class="item1">
         <?php
+            if (!function_exists('currency_format')) {
+                function currency_format($number, $suffix = '₫')
+                {
+                    if (!empty($number)) {
+                        return number_format($number, 0, ',', '.') . "{$suffix}";
+                    }
+                }
+            }
+            
+            if(isset($_POST["search"])){
+                $s=$_POST["txtsearch"];
+                // if($s==""){
+                //     echo "khong tìm thấy";
+                // }
+                // else{
+                    $query1 = "SELECT * FROM sanpham WHERE Tensp LIKE '%$s%'";
+                    $result1=mysqli_query($conn,$query1);
+                    $count1= mysqli_num_rows($result1);
+                    if($count1>0){
+                        if (mysqli_num_rows($result1)!=0){
+                            echo "<tr>";
+                        $dem=1;
+                        while ($rows = mysqli_fetch_array($result1)) {
+                            $dem++; 
+                            echo "<td class='items'>";
+                            echo "<div class='item'>";
+                            ?>
+                            <div class="container1">
+                            <a href="<?php echo URL; ?>sp-info.php?id=<?php echo $rows['Masp']; ?>"><img class="thumb" src="HinhanhSP/<?php echo $rows["Anhbia"]; ?>"></a>
+                            <div class="overlay"><?php echo $rows['Tensp'] ?></div> </div>
+                                <h2 class='sp' style='background-color:white'><?php echo  $rows['Tensp'] ?></h2>
+                                <h3 style='background-color:white;color:#d0021c;font-weight: bold;'><?php echo currency_format($rows['Giatien']) ?></h3>
+                                <?php 
+                                    if (isset($_SESSION['Hinhanh']) && $_SESSION['Hinhanh']){
+                                        echo "<a href='cart.php?Masp=$rows[Masp]'><button type='submit' name='dathang' class='btn btn-success'>Đặt hàng</button></a><br><br>";
+                                    }
+                                    else{
+                                        echo "<a href='login.php'><button type='submit'class='btn btn-success'>Đặt hàng</button></a><br><br>";
+                                    } 
+                                ?>
+                            </div>
+                            </td> 
+                            <?php              
+                            if($dem==5)
+                            {
+                                echo "</tr>";
+                                echo "<tr>";
+                                $dem=1;
+                            }
+                        }
+                        echo "</table>";
+                        }
+                    }
+                }
+            // }
+            else{
                 require 'dt.php';
+            }
         ?>
         
     </div>
